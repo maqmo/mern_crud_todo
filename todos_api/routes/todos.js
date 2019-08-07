d4cbf1078d0187714bd11b072d6f1d5f13e5fb8d
Mohamed <malbgal@gmail.com>
2019-08-07 02:17:23 -0700

    brevity

--- todos_api/routes/todos.js
+++ todos_api/routes/todos.js
@@ -2,22 +2,15 @@
 const express = require('express');
 const router = express.Router();
 const helpers = require('../helper/todos');
-/* requests were passed here from the main server,
-	our first arg in the app.use() line from that server
-	gets prepended so router.get('/' ... is really {first arg}/
-	its short hand
-	the below commented out line works the same as long as the first arg in the app.use( , )
-	is "/" meaning: our callback for any route to .../api/todos/ is (req, res) => { }
-	router.get('/api/todos/', (req,res)=>{
-		res.send("From the todos route");
-	});
-*/
+/* requests to /api/todos are delegated to this router to handle
 
-/*
 instead of :
 	router.get('/', helpers.getTodos)
 	router.put('/', helpers.createTodos)
-can use less code to do the same, as below
+	note that it is established that the first arg above, '/:todoId' is the same
+	as /api/todos/:todoId (the prefix is established since the first argument from the index file was the prefix
+
+use less to do the same:
 */
 router.route('/')
 	.get(helpers.getTodos)
